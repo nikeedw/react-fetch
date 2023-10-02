0a56257fd12b087c5323fed2ea1549b35746bc01
Edward <nexpexploders@gmail.com>
2023-10-02 19:17:01 +0300

    React Fetch start sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Counter from "./components/Counter";
+import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>JavaScript is programming language</div>
+				</div>
+				<div className="post__btns">
+					<button>Delete</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
